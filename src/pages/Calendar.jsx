aaa12e07ac6e8ded87d96c9455dbfa58806fe376
Mysterio-dev <mysterio696@live.ru>
2025-02-06 15:22:09 +0300

    Добавлен новый цвет

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -672,7 +672,7 @@ const App = () => {
         
 
 <Circle
-      colors={[ '#FF2400', '#008000', '#0094ff', '#bc00eb' ]}
+      colors={[ '#FF2400', '#008000', '#0094ff', '#bc00eb', '#0022e2', '#e68200' ]}
       color={eventColor}
       onChange={(color) => {
         setEventColor(color.hex);
